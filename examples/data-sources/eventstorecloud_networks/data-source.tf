780944b92740c1198863aa77471325956fc562ac
Brock Shelton <goabshelto@gmail.com>
2023-12-12 23:13:58 -0500

    re-generate

--- examples/data-sources/eventstorecloud_networks/data-source.tf
+++
@@ -1,8 +0,0 @@
-data "eventstorecloud_networks" "example" {
-  name       = "Example Network"
-  project_id = var.project_id
-}
-
-output "first_network" {
-  value = data.eventstorecloud_networks.example.networks[0]
-}
